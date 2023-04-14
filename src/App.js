008f03b5f1e89bb922115cc0ae1b57dae846703c
Muhammed Mirza Kilic <muhammedmirzakilic@gmail.com>
2023-04-14 20:28:28 +0300

    redirection fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 import Todo from './components/Todo';
+import { useAuth } from './hooks';
 function App() {
+  const { auth } = useAuth();
   return (
     <Routes>
-      <Route path="signup" element={<SignUp />} />
-      <Route path="login" element={<Login />} />
-      <Route path="todo" element={<Todo />} />
+      <Route path="signup" element={auth ? <Navigate to="/todo" /> : <SignUp />} />
+      <Route path="login" element={auth ? <Navigate to="/todo" /> : <Login />} />
+      <Route path="todo" element={auth ? <Todo /> : <Navigate to="/login" />} />
     </Routes>
   );
 }
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState({});
+  const [auth, setAuth] = useState(null);
   return <AuthContext.Provider value={{ auth, setAuth }}>{children}</AuthContext.Provider>;
 };
 
